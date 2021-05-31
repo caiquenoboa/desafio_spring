652adacf18d1a44a9f3bdd9a6c54ebf68c92914c
Gabriel Henrique Leal Garcia <gabriel.lgarcia@mercadolivre.com>
2021-05-31 16:15:38 -0300

    Initial structure

---
+++ src/main/java/com/mercadolivre/desafio_sring/DesafioSringApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.desafio_sring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolivre/desafio_sring/DesafioSringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.desafio_sring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
